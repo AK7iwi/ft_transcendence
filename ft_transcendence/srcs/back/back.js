da51f718ef9903d3905a3cb993b7e79afbc28b53
AK7iwi <atzlts@proton.me>
2025-04-04 10:14:53 +0200

    Back healthy

--- ft_transcendence/srcs/back/back.js
+++ ft_transcendence/srcs/back/back.js
@@ -11,7 +11,6 @@ fastify.get("/health", async (request, reply) => {
   reply.status(200).send({ status: "ok" });
 });
 
-
 const start = async () => {
   try {
     await fastify.listen({ port: PORT, host: "0.0.0.0" });
