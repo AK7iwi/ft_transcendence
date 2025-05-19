fac18d5d1f79a1514e6d950d1d925238073e4d65
AK7iwi <atzlts@proton.me>
2025-05-19 14:06:04 +0200

    Last working version, HTPPS connection work and clean

--- core/srcs/front/vite.config.ts
+++ core/srcs/front/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   server: {
