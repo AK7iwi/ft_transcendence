01137050f0ee931d125544989d45b6731fe8209c
AK7iwi <atzlts@proton.me>
2025-05-25 19:06:10 +0200

    Docker-compose.yml design for microservice

--- core/src/back/src/database/db.index.js
+++
@@ -1,9 +0,0 @@
-const Database = require('better-sqlite3');
-
-// No logging
-const db = new Database(process.env.DB_PATH);
-
-// Enable foreign keys
-db.pragma('foreign_keys = ON');
-
-module.exports = db;
